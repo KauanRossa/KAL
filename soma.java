c74010851bdde241531fcb424aae88252fc011a2
Kauan Rossa <125710216+KauanRossa@users.noreply.github.com>
2023-03-16 09:02:53 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class soma{
+    public static void main(String[] args) {
+        int n1,n2,so;
+        Scanner sc = new Scanner(System.in);
+           System.out.print("Digite o primeiro numero: " );
+           n1=sc.nextInt();
+           System.out.print("Digite o segundo numero: " );
+           n2=sc.nextInt();
+           so=(n1+n2);
+           System.out.print("A soma e de: " +so);
+
+
+    }
+}
